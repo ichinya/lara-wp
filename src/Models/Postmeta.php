616e986d63dda724990c62ccebc6e21a2423df39
Ichi <ichi-perm@yandex.ru>
2024-05-06 18:24:19 +0500

    feat: add model Postmeta and relation, add model Option

---
+++ src/Models/Postmeta.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ichinya\LaraWP\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Postmeta extends Model
+{
+
+    protected $table = 'postmeta';
+    protected $connection = 'wordpress';
+    public $timestamps = false;
+
+}
--- src/Models/User.php
+++ src/Models/User.php
@@ -4,7 +4,17 @@ namespace Ichinya\LaraWP\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $ID
+ * @property string $user_login
+ * @property string $user_pass
+ * @property string $user_nicename
+ * @property string $user_email
+ * @property string $user_url
+ * @property string $display_name
+ */
 class User extends Model
 {
     protected $connection = 'wordpress';
+    public $timestamps = false;
 }
