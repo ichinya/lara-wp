656e95c2189550ec736b50c6f580fd4df8713598
Ichi <ichi-perm@yandex.ru>
2024-05-27 10:58:36 +0500

    add scope status post

--- src/Models/Post.php
+++ src/Models/Post.php
@@ -2,8 +2,11 @@
 
 namespace Ichinya\LaraWP\Models;
 
+use Ichinya\LaraWP\Enums\PostStatuses;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Builder;
 
 class Post extends Model
 {
@@ -16,8 +19,19 @@ class Post extends Model
         return $this->belongsTo(User::class, 'post_author', 'ID');
     }
 
-    public function meta()
+    public function meta(): HasMany
     {
         return $this->hasMany(Postmeta::class, 'post_id', 'ID');
     }
+
+    public function scopePublished(Builder $query): void
+    {
+        $query->where('post_status', 'publish');
+    }
+
+    public function scopeStatus(Builder $query, PostStatuses $status = PostStatuses::Publish): void
+    {
+        $query->where('post_status', $status->value);
+    }
+
 }
